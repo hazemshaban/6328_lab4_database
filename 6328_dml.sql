3e6b8c5c57033e62deae8ed574298a9555c403db
hazem shaban <62478269+hazemshaban@users.noreply.github.com>
2021-11-30 23:36:05 +0200

    Update 6328_dml.sql

--- 6328_dml.sql
+++ 6328_dml.sql
@@ -1,19 +1,23 @@
+-- question (1)
 SELECT customer_name 
 FROM customer
 WHERE customer_name LIKE "Ma%";
 
+-- question (2)
 SELECT order_item.order_id ,sum(quantity) quantity,sum(order_item.quantity*item.unit_price) totalPrice
 FROM order_item
 left JOIN item
 on item.item_id=order_item.item_id
 GROUP BY order_item.order_id;
 
+-- question (3)
 SELECT order_id 
 FROM shipment
 left JOIN warehouse
 on shipment.warehouse_id=warehouse.warehouse_id
 WHERE warehouse.warehouse_city LIKE "Arica";
 
+-- question (4)
 SELECT shipment.warehouse_id ,sum(item.unit_price*order_item.quantity) totalPrice
 FROM shipment
 INNER JOIN order_item 
@@ -23,19 +27,21 @@ on order_item.item_id=item.item_id
 WHERE shipment.warehouse_id=8
 GROUP BY shipment.warehouse_id;
 
+-- question (5)
 SELECT warehouse.warehouse_id,warehouse.warehouse_city,COUNT(shipment.order_id)orderCount
 FROM warehouse
 LEFT JOIN shipment
 on warehouse.warehouse_id=shipment.warehouse_id
 GROUP BY warehouse.warehouse_id;
 
+-- question (6)
 SELECT customer.customer_name,count(`order`.`order_id`)orderCount
 FROM customer
 LEft JOIN `order`
 on customer.customer_id=`order`.`customer_id`
 GROUP by customer.customer_id;
 
-
+-- question (7)
 SELECT item.item_id
 FROM item 
 LEFT JOIN order_item
